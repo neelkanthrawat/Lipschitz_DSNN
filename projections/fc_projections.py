7317d00e6c3508ae911770c6df337a2062146fd1
neelkanthrawat <neelkanthrawat3992@gmail.com>
2024-06-30 16:26:25 +0200

    Add fn for LOT orthogonalisation

--- projections/fc_projections.py
+++ projections/fc_projections.py
@@ -23,10 +23,33 @@ def bjorck_orthonormalize_fc(weights, lipschitz_goal, beta=0.5, iters=15):
     SIAM Journal on Numerical Analysis 8.2 (1971): 358-364.
     We only use the order 1.
     """
-    w = weights / np.sqrt(weights.shape[0] * weights.shape[1])
+    # I am still not sure why are we dividing by sqrt(m*n)
+    w = weights/ np.sqrt(weights.shape[0] * weights.shape[1]) ### what would happen if I dont do this
     for _ in range(iters):
         w_t_w = w.t().mm(w)
         w = (1 + beta) * w - beta * w.mm(w_t_w)
     new_weights = lipschitz_goal * w  
 
     return w # why are we not returning new_weights? it does not make any sense to me!
+
+
+### layer-wise orthogonal training
+def layerwise_orthogonal_fc(weights, lipschitz_goal=1, beta =0.5, iters = 10):
+    """
+    Layer-wise Orthogonal training (LOT)
+    Reference: 1. (Main paper:) Xu et. al NeurIPS-2022-lot-layer-wise-orthogonal-training-on-improving-l2-certified-robustness-Paper-Conference
+            2. Prach et. al. : 1-Lipschitz Layers Compared: Memory, Speed, and Certifiable Robustness, Pg. num 3
+    """
+
+    w = weights / np.sqrt(weights.shape[0] * weights.shape[1]) ### what would happen if I dont do this
+    y = weights.t().mm(w)
+    z = torch.eye(y.shape[0])
+    for _ in range(iters):
+        zi_yi = z.mm(y)
+        diff_mat = 3 * torch.eye(zi_yi.shape[0]) - zi_yi
+        y = beta * y.mm(diff_mat)
+        z = beta * diff_mat.mm(z)
+
+    new_weights = lipschitz_goal * w 
+    return new_weights
+
